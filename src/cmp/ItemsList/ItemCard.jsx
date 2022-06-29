b0a1b1ce041a30878f4fc3ab743a2b0bd830d22d
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-06-29 11:23:56 +0300

    Modif

--- src/cmp/ItemsList/ItemCard.jsx
+++ src/cmp/ItemsList/ItemCard.jsx
@@ -1,17 +1,17 @@
 import { React } from "react";
-import { NavLink, Link } from "react-router-dom";
-import pic from "../../img/peppa.jpg"
 
-const ItemCard = () => {
-    return (
-        <div className="item__card">
-            <img className="item__pic" src={pic}></img>
-            <div className="item__info">
-                <div className="item__name">Свинка</div>
-                <h1>1000 ₽</h1>
-            </div>
-            <button className="btn">В корзину</button>
-        </div>
-    );
+const ItemCard = ({ id, title, description, imageUrl, number, price }) => {
+  return (
+    <div className="item__card">
+      <img className="item__pic" src={imageUrl} />
+      <div className="item__info">
+        <div>{title}</div>
+        <div className="item__description">{description}</div>
+        <div>Количество: {number} штук</div>
+        <div>{price} ₽</div>
+      </div>
+      <button className="btn">В корзину</button>
+    </div>
+  );
 };
 export default ItemCard;
--- src/cmp/ItemsList/ItemsList.jsx
+++ src/cmp/ItemsList/ItemsList.jsx
@@ -1,28 +1,17 @@
 import { React } from "react";
-import { NavLink, Link } from "react-router-dom";
 import ItemCard from "./ItemCard";
+import { observer } from "mobx-react-lite";
+import { productsStore } from "../../store/productsStore";
 
-const ItemsList = () => {
-    return (
-        <div className="main__container">
-            <div className="items__list">
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
-            </div>
-        </div>
-
-    );
-};
+const ItemsList = observer(() => {
+  return (
+    <div className="main__container">
+      <div className="items__list">
+        {productsStore.products.map((product, index) => (
+          <ItemCard key={index} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+});
 export default ItemsList;
